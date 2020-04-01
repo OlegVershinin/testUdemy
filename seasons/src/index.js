9b684565653c03c3956d095f3816e391fd585590
OlegVershinin <marjag12@gmail.com>
2020-04-01 17:51:39 +0300

    initial commit

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,8 +1,33 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-const App = () => {
-  return <div>Опять, привет сосед!</div>
+// const App = () => {
+//   return <div>Опять, привет сосед!</div>
+// }
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = { lat: null, errorMessage: '' }
+
+    window.navigator.geolocation.getCurrentPosition(
+      position => {
+        this.setState({ lat: position.coords.latitude })
+      },
+      err => {
+        this.setState({ errorMessage: err.message })
+      }
+    )
+  }
+  render() {
+    return (
+      <div>
+        Широта: {this.state.lat}
+        <p>Error: {this.state.errorMessage}</p>
+      </div>
+    )
+  }
 }
 
 ReactDOM.render(<App />, document.querySelector('#root'))
